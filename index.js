f909607eec8ba53b8a641bd6b3df60a56650e6b7
kileras <kileras@gmail.com>
2013-10-14 00:15:29 +0200

    Moved to modules. Added file resolver

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*jslint node:true, devel: true, plusplus: true, vars: true */
-var sinxelo = require("./sinxelo");
+var sinxelo = require("sinxelo");
 
 // Initialize the server
 sinxelo.start('config.json');
